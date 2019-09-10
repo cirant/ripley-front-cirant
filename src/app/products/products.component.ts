b2fbdda98137fcda2d9c959849f13f284dce3e68
Pablo Cirant <cirant3002@gmail.com>
2019-09-09 21:15:41 -0300

    product component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product/product.service';
+
+interface Details {
+  attributes: Array<any>;
+  thumbnail: String;
+}
 
 @Component({
   selector: 'app-products',
@@ -7,9 +13,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  list:Array<any>;
+  loading:boolean = false;
+  details:Details;
+  title:String;
+
+  constructor(private productService: ProductService) {
+    this.setDefault();
+  }
 
   ngOnInit() {
+    this.getProducts();
+  }
+
+  setDefault(){
+    this.title = 'productos Ripley';
+    this.details = null;
+  }
+
+  getProducts() {
+    this.loading = true;
+    this.productService.getProductList().subscribe(res => {
+      this.loading = false;
+      this.list = res['data'];
+    }, err => {
+      this.loading = false;
+      this.productService.handlerError();
+    });
+  }
+
+  getDetails(sku):void {
+    this.loading = true;
+    this.productService.getProductDetails(sku).subscribe(res => {
+      this.loading = false;
+      const attributes = res['data']['attributes'].map(item => {
+        return {
+          name: item.name,
+          value: item.value
+        }
+      });
+
+      this.details = {
+        attributes: attributes,
+        thumbnail: res['data']['thumbnailImage']
+      };
+
+      this.title = res['data']['shortDescription'];
+    }, err => {
+      this.loading = false;
+      this.productService.handlerError();
+    });
+  }
+
+  goBack():void {
+    this.getProducts();
+    this.setDefault();
   }
 
 }
